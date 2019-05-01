cc2a66dc3d8e5221460f9e3e536c9ad1338ad863
Justin Hart <jhart@onyxraven.com>
2019-05-01 11:37:20 -0700

    flags and package updates

--- server/udp.rb
+++ server/udp.rb
@@ -1,9 +1,13 @@
 require 'socket'
+
 BasicSocket.do_not_reverse_lookup = true
+
 # Create socket and bind to address
 client = UDPSocket.new
-client.bind(nil, 8125)
+client.bind('0.0.0.0', 8125)
+
 loop do
-  data, addr = client.recvfrom(1024) # if this number is too low it will drop the larger packets and never give them to you
+  # if this number is too low it will drop the larger packets and never give them to you
+  data, addr = client.recvfrom(1024)
   puts "From addr: '%s', msg: '%s'" % [addr.join(','), data]
 end
